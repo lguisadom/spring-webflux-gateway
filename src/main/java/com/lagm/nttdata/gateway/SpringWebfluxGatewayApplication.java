8e130ca81b5b283b09b4c2531c6b89d0b6a3cbf9
Luis Guisado <lguisadom@gmail.com>
2022-03-08 16:36:57 -0500

    Start

---
+++ src/main/java/com/lagm/nttdata/gateway/SpringWebfluxGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lagm.nttdata.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagm/nttdata/gateway/SpringWebfluxGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagm.nttdata.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
